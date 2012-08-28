94540c256a3ab6c7ee3656c5761dba77993b6034
Ruben Epong <rubenjohne@gmail.com>
2012-08-28 14:57:34 -0700

    blueprint stylesheet added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  # return a title on a per-page basis
+  def title
+    base_title ="Tadashi Shoji"
+    if @title.nil?
+      base_title
+    else
+      "#{@title} | #{base_title}"
+    end
+    
+  end
+  
 end
