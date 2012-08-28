afe8ec93599d6bcc17612bb41bfd2fd6cd0291af
Ruben Epong <rubenjohne@gmail.com>
2012-08-28 14:23:05 -0700

    Done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,9 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def collections
+    @title = "Collections"
   end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,33 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
-    it "returns http success" do
+    it "should be successful" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", :content => "Home | Tadashi Shoji")
+    end
+    
   end
 
   describe "GET 'collections'" do
-    it "returns http success" do
+    
+    it "should be successful" do
       get 'collections'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'collections'
+      response.should have_selector("title", :content => "Collections | Tadashi Shoji")
+    end
+
   end
 
 end
