9ac3737581844f81cbc77ce022555196f82cf7f3
Ruben Epong <rubenjohne@gmail.com>
2013-02-26 17:19:48 -0800

    updated catalog navigation links

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,9 @@
 class Collection < ActiveRecord::Base
   attr_accessible :collection, :video_url
   has_many :styles, :order => "position ASC"
+  
+  def has_video?
+    video_url != '' && !video_url.nil?
+  end
+  
 end
